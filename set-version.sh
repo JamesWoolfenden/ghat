bd0a4ade83642f6581127ff92e171c5918074b96
James Woolfenden <james.woolfenden@gmail.com>
2023-06-15 16:44:06 +0100

    versioning

--- set-version.sh
+++ set-version.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 latestTag=$(git describe --tags)
 echo "Updating version file with new tag: $latestTag"
-echo "package ghat" > src/version.go
+echo "package version" > src/version/version.go
 echo "" >> src/version/version.go
 echo "const Version = \"$latestTag\"" >> src/version/version.go
