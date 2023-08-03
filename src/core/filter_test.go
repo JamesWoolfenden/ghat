a3550ead2db60591709c629c46a6d80ef5828740
James Woolfenden <james.woolfenden@gmail.com>
2023-08-03 15:15:37 +0100

    fix test

--- src/core/filter_test.go
+++ src/core/filter_test.go
@@ -16,7 +16,45 @@ func TestGetReleases(t *testing.T) {
 	delay := 14
 	zero := 0
 	var empty map[string]interface{}
-	var want map[string]interface{}
+	want := map[string]interface{}{
+		"tarball_url":  "https: //api.github.com/repos/JamesWoolfenden/test-data-action/tarball/v0.0.1",
+		"zipball_url":  "https: //api.github.com/repos/JamesWoolfenden/test-data-action/zipball/v0.0.1",
+		"assets_url":   "https:  //api.github.com/repos/JamesWoolfenden/test-data-action/releases/109328421/assets",
+		"id":           109328421,
+		"draft":        false,
+		"created_at":   "2023-06-21T06:59:22Z",
+		"published_at": "2023-06-21T06:59:51Z",
+		"assets":       []interface{}{},
+		"html_url":     "https: //github.com/JamesWoolfenden/test-data-action/releases/tag/v0.0.1",
+		"author": map[string]interface{}{
+			"avatar_url":          "https://avatars.githubusercontent.com/u/1456880?v=4",
+			"url":                 "https://api.github.com/users/JamesWoolfenden",
+			"type":                "User",
+			"followers_url":       "https://api.github.com/users/JamesWoolfenden/followers",
+			"organizations_url":   "https://api.github.com/users/JamesWoolfenden/orgs",
+			"starred_url":         "https://api.github.com/users/JamesWoolfenden/starred{/owner}{/repo}",
+			"events_url":          "https://api.github.com/users/JamesWoolfenden/events{/privacy}",
+			"login":               "JamesWoolfenden",
+			"id":                  1456880,
+			"node_id":             "MDQ6VXNlcjE0NTY4ODA=",
+			"gravatar_id":         "",
+			"html_url":            "https://github.com/JamesWoolfenden",
+			"following_url":       "https://api.github.com/users/JamesWoolfenden/following{/other_user}",
+			"gists_url":           "https://api.github.com/users/JamesWoolfenden/gists{/gist_id}",
+			"subscriptions_url":   "https://api.github.com/users/JamesWoolfenden/subscriptions",
+			"repos_url":           "https://api.github.com/users/JamesWoolfenden/repos",
+			"received_events_url": "https://api.github.com/users/JamesWoolfenden/received_events",
+			"site_admin":          false,
+		},
+		"node_id":          "RE_kwDOJyIXLs4GhDgl",
+		"tag_name":         "v0.0.1",
+		"name":             "Test",
+		"prerelease":       false,
+		"body":             "",
+		"url":              "https://api.github.com/repos/JamesWoolfenden/test-data-action/releases/109328421",
+		"upload_url":       "https: //uploads.github.com/repos/JamesWoolfenden/test-data-action/releases/109328421/assets{?name,label}",
+		"target_commitish": "main",
+	}
 
 	result := map[string]interface{}{
 		"tarball_url":  "https: //api.github.com/repos/JamesWoolfenden/test-data-action/tarball/v0.0.1",
