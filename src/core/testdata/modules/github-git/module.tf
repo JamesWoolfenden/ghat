1869d48b7d7e68c6e782b66c60db09f012bfde10
James Woolfenden <james.woolfenden@gmail.com>
2023-08-17 10:09:56 +0100

    fix hash lookup

---
+++ src/core/testdata/modules/github-git/module.tf
@@ -0,0 +1,6 @@
+module "diskencryptionset" {
+  source      = "git::https://github.com/JamesWoolfenden/terraform-azurerm-diskencryptionset.git?ref=fc0b830997dd820476a7ad5e4b6ef2dcbdc766d7"
+  common_tags = var.common_tags
+  location    = var.location
+  rg_name     = var.resource_group_name
+}
